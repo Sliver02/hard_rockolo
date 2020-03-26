bb7a18da069fe42a77e82183e5050cc739b1928d
JPanzera <J.Panzera@eu.aswatson.com>
2020-03-26 15:18:51 +0100

    map finished and polished

--- src/views/sections/custom-map.js
+++ src/views/sections/custom-map.js
@@ -11,13 +11,29 @@ class CustomMap extends BaseView {
     
     constructor() {
         super();
+        this.map = undefined;
+        this.infoShow = false;
         this.geojson = {
             'type': 'FeatureCollection',
             'features': [
                 {
                     'type': 'Feature',
                     'properties': {
-                        'message': 'Monte Ricco',
+                        'infobox': false,
+                        'icon': 'local_parking',
+                        'iconSize': [50, 50]
+                    },
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [12.374105053512778, 46.427489974366836]
+                    }
+                },
+                {
+                    'type': 'Feature',
+                    'properties': {
+                        'infobox': true,
+                        'img': '',
+                        'description': 'Truckeroo brings dozens of food trucks, live music, and games to half and M Street SE (across from Navy Yard Metro Station) today from 11:00 a.m. to 11:00 p.m.',
                         'iconSize': [50, 50]
                     },
                     'geometry': {
@@ -28,8 +44,10 @@ class CustomMap extends BaseView {
                 {
                     'type': 'Feature',
                     'properties': {
-                        'message': 'Main Festival',
-                        'iconSize': [60, 60]
+                        'infobox': true,
+                        'img': '',
+                        'description': 'Truckeroo brings dozens of food trucks, live music, and games to half and M Street SE (across from Navy Yard Metro Station) today from 11:00 a.m. to 11:00 p.m.',
+                        'iconSize': [80, 80]
                     },
                     'geometry': {
                         'type': 'Point',
@@ -44,18 +62,33 @@ class CustomMap extends BaseView {
     render() {
         return html`
             <div id="map" class="map"></div>
+            <div class="map__info">
+                <ul>
+                    <dt></dt>
+                    <li><strong>Cittá:</strong> Pieve di Cadore, 32044, BL</li>
+                    <li><strong>Main stage:</strong> <a href="" @click="${() => this.flyToMarker(2)}">Ex pattinaggio, Parco Roccolo</a></li>
+                    <li><strong>Forti stage:</strong> <a href="" @click="${() => this.flyToMarker(1)}">Forte di Monte Ricco</a></li>
+                    <li><strong>Parcheggio:</strong> <a href="" @click="${() => this.flyToMarker(0)}">Piazzale degli Aplini</a></li>
+                </ul>
+            </div>
         `;
     }
 
     firstUpdated() {
         this.initMap();
+        this.map.resize();
     }
 
+    // inizializzo la mappa con valori di default, tra cui lo stile creato sul portale di mapbox
+    // limiti e zoom iniziale, e centramento iniziale (basato sulla media dei marker)
     initMap() {
-        var map = new mapboxgl.Map({
+        const mapCenter = this.averageGeolocation(this.geojson.features);
+
+        this.map = new mapboxgl.Map({
             container: 'map',
-            center: [12.37653481071527, 46.427623023499734],
+            center: mapCenter,
             zoom: 15,
+            minZoom: 5,
             style: 'mapbox://styles/sliver02/ck85u1r900hv11iqc9x6haf0f',
             hash: true,
             accessToken: 'pk.eyJ1Ijoic2xpdmVyMDIiLCJhIjoiY2s4NXBmcDhqMDg2dzNkcGVvbTRoNDd3OCJ9.c74ira4lmsYsoPKkxXN5Ew',
@@ -70,28 +103,96 @@ class CustomMap extends BaseView {
             }
         });
 
-        this.geojson.features.forEach(function(marker) {
-            // create a DOM element for the marker
+        this.renderMarkers(this.map);
+
+        return this.map;
+    }
+
+    renderMarkers() {
+        this.geojson.features.forEach( (marker, index) => {
+            
             var el = document.createElement('div');
             el.className = 'marker';
-            el.style.backgroundImage =
-            'url(https://placekitten.com/g/' +
-            marker.properties.iconSize.join('/') +
-            '/)';
+
+            if (marker.properties.icon === 'local_parking') {
+                el.classList.add('marker--parking');
+                el.style.backgroundImage = `url(../../assets/img/icons/${marker.properties.icon}.svg)`;
+            } else {
+                el.style.backgroundImage = `url(../../assets/img/icons/hr_stemma.svg)`;
+            }
+
             el.style.width = marker.properties.iconSize[0] + 'px';
             el.style.height = marker.properties.iconSize[1] + 'px';
+
+            var coordinates = marker.geometry.coordinates.slice();
+            var description = `
+                <img src="${marker.properties.img}"/>
+                <p> ${marker.properties.description} </p>
+            `;
+           
+            // el.addEventListener('click', (e) => {
+            //     this.map.flyTo({ center: coordinates });
+            // });         
+
+            if (marker.properties.infobox) {
+                var popup = new mapboxgl.Popup({ offset: marker.properties.iconSize[0] / 2 })
+                .setLngLat(coordinates)
+                .setHTML(description);
+
+                new mapboxgl.Marker(el)
+                .setLngLat(coordinates)
+                .setPopup(popup)
+                .addTo(this.map);
+            } else {
+                new mapboxgl.Marker(el)
+                .setLngLat(coordinates)
+                .addTo(this.map);
+            }
              
-            el.addEventListener('click', function() {
-                window.alert(marker.properties.message);
-            });
-             
-            // add marker to map
-            new mapboxgl.Marker(el)
-            .setLngLat(marker.geometry.coordinates)
-            .addTo(map);
+        });
+    }
+
+    flyToMarker(markerID) {
+        var coordinates = this.geojson.features[markerID].geometry.coordinates.slice();
+
+        if (this.map != undefined) {
+            this.map.flyTo({ center: coordinates, zoom: 18, });
+        }
+    }
+
+    averageGeolocation(features) {
+
+
+        if (features.length === 1) {
+            return features[0];
+        }
+
+        let x = 0.0;
+        let y = 0.0;
+        let z = 0.0;
+
+        features.forEach(feature => {
+            let latitude = feature.geometry.coordinates[0] * Math.PI / 180;
+            let longitude = feature.geometry.coordinates[1] * Math.PI / 180;
+
+            console.log(latitude);
+
+            x += Math.cos(latitude) * Math.cos(longitude);
+            y += Math.cos(latitude) * Math.sin(longitude);
+            z += Math.sin(latitude);
         });
 
-        return map;
+        let total = features.length;
+
+        x = x / total;
+        y = y / total;
+        z = z / total;
+
+        let centralLongitude = Math.atan2(y, x);
+        let centralSquareRoot = Math.sqrt(x * x + y * y);
+        let centralLatitude = Math.atan2(z, centralSquareRoot);
+
+        return [centralLatitude * 180 / Math.PI, centralLongitude * 180 / Math.PI];
     }
 }
 
