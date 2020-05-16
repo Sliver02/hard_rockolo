adea1b5192a0323a25a638ff7d8ecbc823344e0d
JPanzera <J.Panzera@eu.aswatson.com>
2020-05-16 10:50:56 +0200

    small fixes

--- src/views/sections/news-section.js
+++ src/views/sections/news-section.js
@@ -24,7 +24,7 @@ class NewsSection extends LitElement {
     }
 
     handleClick(articleID) {
-        this.toggleOverlay();
+        this.toggleOverlay('articleID');
     }
           
     render() {
