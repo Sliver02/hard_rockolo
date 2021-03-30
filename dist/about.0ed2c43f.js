8721000a36ba83c05cb3912a3b5435fe981a3c95
Jacopo <kiuby09@gmail.com>
2021-03-30 14:51:07 +0200

    image web optimization

---
+++ dist/about.0ed2c43f.js
@@ -0,0 +1,46 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{30:function(e,t,i){(t=i(2)(!1)).push([e.i,"@-moz-keyframes loadbar{from{-moz-transform:scaleX(100%)}to{-moz-transform:scaleX(1)}}@-webkit-keyframes loadbar{from{-webkit-transform:scaleX(0)}to{-webkit-transform:scaleX(1)}}@keyframes loadbar{from{transform:scaleX(0)}to{transform:scaleX(1)}}@-moz-keyframes slide-left{from{-moz-transform:translate(100%)}to{-moz-transform:translate(0)}}@-webkit-keyframes slide-left{from{-webkit-transform:translate(100%)}to{-webkit-transform:translate(0)}}@keyframes slide-left{from{transform:translate(100%)}to{transform:translate(0)}}@-moz-keyframes slide-right{from{-moz-transform:translate(-100%)}to{-moz-transform:translate(0)}}@-webkit-keyframes slide-right{from{-webkit-transform:translate(-100%)}to{-webkit-transform:translate(0)}}@keyframes slide-right{from{transform:translate(-100%)}to{transform:translate(0)}}@-moz-keyframes hover{from{-moz-transform:translate(0, 0)}to{-moz-transform:translate(0, 15px)}}@-webkit-keyframes hover{from{-webkit-transform:translate(0, 0)}to{-webkit-transform:translate(0, 15px)}}@keyframes hover{from{transform:translate(0, 0)}to{transform:translate(0, 15px)}}.icon::before{display:inline-block;font-style:normal;font-variant:normal;text-rendering:auto;-webkit-font-smoothing:antialiased}.fa-chevron-right::before{font-family:'Font Awesome 5 Free';content:'\\f054';display:inline-block;vertical-align:middle;font-weight:900}.fa-chevron-left::before{font-family:'Font Awesome 5 Free';content:'\\f053';display:inline-block;vertical-align:middle;font-weight:900}.fa-times-circle::before{font-family:'Font Awesome 5 Free';content:'\\f057';display:inline-block;vertical-align:middle;font-weight:900}.fa-book-navigation::before{font-family:'Font Awesome 5 Free';content:'\\f0c9';display:inline-block;vertical-align:middle;font-weight:510}.fa-chevron-down::before{font-family:'Font Awesome 5 Free';content:'\\f078';display:inline-block;vertical-align:middle;font-weight:900}.fa-facebook-square::before{font-family:'Font Awesome 5 Brands';content:'\\f082';display:inline-block;vertical-align:middle}.fa-instagram::before{font-family:'Font Awesome 5 Brands';content:'\\f16d';display:inline-block;vertical-align:middle}*{box-sizing:border-box;margin:0;padding:0}a{color:white;text-decoration:none;outline:0}a:hover{color:white;text-decoration:none}a:active,a:focus{outline:0;border:none;-moz-outline-style:none}h1,h2,h3{font-family:'Montserrat', sans-serif;text-transform:uppercase;font-weight:600}h1{font-size:4rem;line-height:0.8;letter-spacing:-0.2rem;margin-bottom:30px}h2{line-height:0.8;margin-bottom:15px}ul{list-style-type:none}hr{margin:30px 0}span{font-size:0.8rem}nav{display:flex;flex-wrap:wrap;justify-content:space-between;align-items:center;position:fixed;width:100%;z-index:10;padding:20px 40px;transition:background 0.4s ease-out}.btn{cursor:pointer;display:inline-block;padding:0.8rem 1.5rem;transition:all 0.3s;text-transform:uppercase}.btn--active,.btn:hover,.btn:active{color:#333;background:white}.btn--active:hover,.btn:hover:hover,.btn:active:hover{background:white}.section{display:flex;align-items:center;justify-content:center;flex-direction:column;width:100%;padding:80px 0}.section__container{display:flex;flex-direction:column;align-items:center}@media (min-width: 600px){.section__container{width:100%;max-width:1400px}}@media (min-width: 1200px){.section__container{padding:0 20px}}.section__title{width:100%;align-self:left;padding:0 20px;align-self:flex-start;max-width:1400px}@media (min-width: 1200px){.section__title{padding:0}}.section__content{width:100%;max-width:1400px;padding:0 20px}@media (min-width: 1200px){.section__content{padding:0}}.section--dark{background:#333;color:#ddd}.--no-padding{padding:0}@media (min-width: 900px){.--display-tablet-down{display:none}}.--display-tablet-up{display:none}@media (min-width: 900px){.--display-tablet-up{display:block}}.section__container{max-width:1000px}.about__img-wrap{display:flex;align-items:center;justify-content:center;overflow:hidden;width:100%;height:100%}.about__img{display:flex;align-items:center;justify-content:center;width:100%;height:100%;position:relative}.about__img img{position:absolute}.about__img-wrap{height:auto;min-height:300px;margin-bottom:50px}.about__img img{width:100%;min-width:500px;min-height:100%}.about__desc{margin-bottom:50px;padding:0 20px}@media (min-width: 1200px){.about__desc{padding:0}}iframe{width:100%}@media (min-width: 600px){iframe{min-height:500px}}\n",""]),e.exports=t},33:function(e,t,i){"use strict";i.r(t);var a=i(0),o=(i(23),i(30)),n=i.n(o);class r extends a.a{static get styles(){return Object(a.b)([n.a])}static get properties(){return{}}constructor(){super()}render(){return a.c`
+
+      <page-header 
+        img="DSC_0664"
+        height="30">
+      </page-header>
+
+      
+      <div class="section">
+        <div class="section__container">
+          <h1 class="section__title">About Us</h1>
+          
+          <div class="about">
+            <div class="about__img-wrap">
+              <div class="about__img">
+                <img src="./assets/images/header/DSC_0003.jpg"/>
+              </div>
+            </div>
+            <p class="about__desc">
+              Hard Rockolo è un festival musicale che prende vita a Pieve di Cadore, in un luogo magico situato in mezzo alle Dolomiti Bellunesi, patrimonio dell'Unesco. 
+              <br>
+              <br>
+              Il nostro obbiettivo è quello di portare della buona musica, vissuta nella maniera più autentica e genuina del festival, promuovendo gli artisti locali e regalando un'esperienza indeterminable a chi la ama.
+            </p>
+
+            <div class="about__img-wrap">
+              <div class="about__img">
+                <img src="./assets/images/header/cadore-212.jpg"/>
+              </div>
+            </div>
+            <p class="about__desc">
+              Hard Rockolo Festival, in collaborazione con la Pro Loco Tiziano, è realizzato interamente da giovani volontari, amanti della musica, e proprio per questo sa come regalarvi vere emozioni.
+              <br>
+              <br>
+              Unitevi a noi nel riaccendere le notti tra le Dolomiti, godetevi il panorama mozzafiato sul Lago centro Cadore e preparatevi a scatenarvi davanti al nostro Palco!
+            </p>
+
+          </div>
+
+          <iframe height="300" src="https://www.youtube.com/embed/_ZayLGaoXFY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        </div>
+      </div>
+
+    `}firstUpdated(){document.querySelector("html").scrollTop=0}}customElements.define("about-view",r)}}]);
+//# sourceMappingURL=about.0ed2c43f.js.map
---
+++ dist/merch.af4269a1.js
@@ -0,0 +1,9 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{36:function(e,r,s){"use strict";s.r(r);var t=s(29);class n extends t.a{createRenderRoot(){return this}}s(23),s(8);customElements.define("merch-view",class extends n{static get properties(){return{}}constructor(){super()}render(){return t.b`
+      <page-header 
+        img="DSC_0035"
+        height="300">
+      </page-header>
+
+      <merch-slider></merch-slider>
+    `}firstUpdated(){window.scrollTo(0,0)}})}}]);
+//# sourceMappingURL=merch.af4269a1.js.map
